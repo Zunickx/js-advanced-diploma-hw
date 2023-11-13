c570bf8b9e5e07f7f6e823650891275c410b6931
Zunickx <Zunickx@gmail.com>
2023-11-13 11:43:58 +0300

    Create file GameState

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,8 +1,17 @@
-import createMatrix, { findIndex, findRangeAttack, findRangeMove } from './createMatrix';
+import PositionedCharacter from './PositionedCharacter'; 
+import Character from './Character';
 
-export default class GameState {
-  static from(object) {
-    // TODO: create object
-    return null;
-  }
+export default class GameState { // принимаем объект с полями theme (тема), characters (персонажи) и userTeam (команда пользователя). 
+  // Метод from создает новый объект GameState с этими полями и возвращает его.
+	static from({ characters, theme, userTeam }: {
+		theme: string,
+		characters: PositionedCharacter[],
+		userTeam: Character[]
+	}) {
+		return {
+			theme,
+			characters,
+			userTeam
+		};
+	}
 }
